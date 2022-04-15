400d9be3c38cb6909bb1fbdfc9b999ea38a6e5a3
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-04-15 23:53:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(" This is Asynchronous operation 1")
+
+console.log(" This is Asynchronous operation 2")
+
+setTimeout(()=>{
+    
+console.log(" This is Asynchronous operation 3")
+
+},2000)
+console.log(" This is Asynchronous operation 4")
+
+console.log(" This is Asynchronous operation 5")
+
